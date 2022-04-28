fe4dd9e41cbaea25badfa80f0d644e4e457d08f6
Matt McGee <mlmcgeenc@gmail.com>
2022-04-28 14:09:30 -0400

    build password object

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,29 @@
 // Assignment code here
-var password  = {
-  length: null,
-  includeLowerCase: null,
-  includeUpperCase: null,
-  includeNumbers: null,
-  includeSpecialCharacters: null,
-}
+var password = {
+	length: null,
+	includeLowerCase: null,
+	includeUpperCase: null,
+	includeNumbers: null,
+	includeSpecialCharacters: null,
+	assignPasswordLength: function () {},
+	userAssignLowerCase: function () {},
+	userAssignUpperCase: function () {},
+	userAssignIncludeNumbers: function () {},
+	userAssignSpecialCharacters: function () {},
+};
 var generatePassword = function () {
-  return "My New Password"
-}
+	password.assignPasswordLength ();
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+	var password = generatePassword();
+	var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+	passwordText.value = password;
 }
 
 // Add event listener to generate button
